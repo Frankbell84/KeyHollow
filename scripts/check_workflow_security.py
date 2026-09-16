a45c378d8817517b4be6e5a700c3e4220ef8577e
Frankbell84 <214388681+Frankbell84@users.noreply.github.com>
2026-09-16 08:16:40 -0400

    Prepare Build 46 regression fixes

--- scripts/check_workflow_security.py
+++ scripts/check_workflow_security.py
@@ -209,7 +209,7 @@ PRIVILEGED_SOURCE_SHA256 = {
         "232f0b11e50aba390d23206692ba3c3ec9e72fa83af2dd30876bc3486188ee9d"
     ),
     "project.yml": (
-        "49f6ba8e66ee658b6e6e37db4ffab305f4593740576219619275e28875572a29"
+        "d5cf06262fc212ad8aba8bfc547168183a9f31113c63ecc9e453bd955b9f32af"
     ),
 }
 
